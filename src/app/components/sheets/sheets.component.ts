17e4e198e747460eb83febefade06e9f495ee566
khamui <khamui@me.com>
2021-03-07 17:47:42 +0000

    close #8: installing handsontable for angular (basic sample data)

--- src/app/components/sheets/sheets.component.ts
+++ src/app/components/sheets/sheets.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Handsontable from 'handsontable';
+import { HotTableRegisterer } from '@handsontable/angular';
 
 @Component
 ({
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['sheets.component.sass']
 })
 export class SheetsComponent
-{}
+{
+  data = [
+    { id: 1, name: 'Ted Right', address: '' },
+    { id: 2, name: 'Frank Honest', address: '' },
+    { id: 3, name: 'Joan Well', address: '' },
+    { id: 4, name: 'Gail Polite', address: '' },
+    { id: 5, name: 'Michael Fair', address: '' },
+  ]
+  private hotRegisterer = new HotTableRegisterer();
+  hotSettings: Handsontable.GridSettings = {
+    data: this.data,
+    colHeaders: true,
+    width: '100%',
+    height: '100%',
+    contextMenu: true,
+    licenseKey: 'non-commercial-and-evaluation'
+  };
+
+  clear()
+  {
+    console.log(this.hotRegisterer.getInstance('myHot').getData());
+  }
+}
